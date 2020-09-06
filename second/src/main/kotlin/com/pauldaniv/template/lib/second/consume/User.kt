5356213fb104623bb4eb0723ff2dd09586576939
Paul <paulsegfault@gmail.com>
2020-09-06 12:47:50 +0300

    Cleanup

--- second/src/main/kotlin/com/pauldaniv/template/lib/second/consume/User.kt
+++ second/src/main/kotlin/com/pauldaniv/template/lib/second/consume/User.kt
@@ -2,7 +2,6 @@ package com.pauldaniv.template.lib.second.consume
 
 import com.pauldaniv.template.lib.first.util.TestUtil
 
-
 data class User(val first: String, val second: String) {
   fun doTest(): String {
     return TestUtil.join(first, second)
